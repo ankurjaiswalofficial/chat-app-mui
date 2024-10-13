0a1a52bdf4e22ccc0ebc2c3aeb9c88238c8b15c6
Ankur Jaiswal <ankurjaiswalofficial@gmail.com>
2024-10-13 17:43:41 +0000

    added heading and meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Box, Container, Paper } from '@mui/material';
+import { Box, Container, Paper, Typography } from '@mui/material';
 import { store } from './store/store';
 import ChatWindow from './components/ChatWindow';
 import MessageInput from './components/MessageInput';
@@ -23,6 +23,7 @@ function App() {
         <CssBaseline />
         <Container maxWidth="sm" sx={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
           <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', mt: 2 }}>
+            <Typography sx={{fontWeight: "bold"}} >BOTTED-Chat APP MUI</Typography>
             <Paper elevation={3} sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
               <ChatWindow />
               <MessageInput />
